a352ab164a5e14346f6e18ad2ce147b13d61fe9e
Leiv Fredrik Sundgaard Berge <lfsb@buypass.no>
2018-04-29 12:24:24 +0200

    Init project

---
+++ README.md
@@ -0,0 +1,18 @@
+# Welcome
+
+This is a recruitment project for front end developer applying to Buypass.
+
+## Prerequisites
+* Node
+* Yarn or npm
+* git
+* Github account
+
+# Getting started
+Get up and running by cloning the project and run `yarn install` to install all dependencies. To start the development server run `yarn start`. This will launch your default web browser at localhost:3000 with HMR enabled.
+
+# Share your solution
+Commit your changes and 
+
+
+Good luck!
---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPots } from "./state/actions";
+import "./App.css";
+
+class App extends Component {
+  componentWillMount() {
+    this.props.fetchArticles();
+  }
+  render() {
+    return (
+      <main className="App">
+        <header className="App-header">
+          <h1 className="App-title">Buypass</h1>
+        </header>
+        <section className="App-content">
+          <p>
+            Vi har gjort klar en Redux store, med en action for å hente data fra
+            et API. Du skal trigge den actionen, og hente ut innholdet i feltet
+            `pots`.
+          </p>
+          <p>
+            Scopet inkluderer å koble dette viewet til Redux, å iterere over
+            dataen og å lage nødvendige komponenter for å vise dataen i
+            browseren.
+          </p>
+          <p>
+            Det er opp til deg hvordan du ønsker å strukturere komponenter,
+            style viewet og hvordan du presenterer dataene. Det er også opp til
+            deg om du ønsker å ta i bruk eksterne bibliotek ut over de som
+            allerede finnes i prosjektet. Vi har laget en wireframe som en grov
+            skisse. Vi ønsker at det normalt vises potter fra fire spill, og at
+            man kan expande det til å vise alle pottene inne i samme viewet.
+          </p>
+          <img alt="App wireframe" src={"./BUYPASS_RECRUITING.png"} />
+          <h2>Lykke til!</h2>
+        </section>
+      </main>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchArticles: () => dispatch(fetchPots())
+  };
+};
+
+export default connect(undefined, mapDispatchToProps)(App);
